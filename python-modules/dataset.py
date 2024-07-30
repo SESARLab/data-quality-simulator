692e7523632fd06b23bf8c2ef6014e53c7619ed0
Marco Luzzara <marco.luzzara@hotmail.it>
2024-07-30 14:48:01 +0200

    add redwine and HR analytics datasets

--- python-modules/dataset.py
+++ python-modules/dataset.py
@@ -13,3 +13,11 @@ def low_variability_10k():
 def avocado():
     import pandas as pd
     return pd.read_csv('datasets/avocado.csv', index_col=[0])
+
+def IBM_HR_Analytics_employee_attrition():
+    import pandas as pd
+    return pd.read_csv('datasets/IBM_HR_Analytics_employee_attrition.csv')
+
+def red_wine_quality():
+    import pandas as pd
+    return pd.read_csv('datasets/red_wine_quality.csv')
