050e6dcfa492a22c20913b1083bebe2f942b3df8
Marco Luzzara <marco.luzzara@hotmail.it>
2024-09-18 00:11:53 +0200

    [wip] add entropy metric

--- python-modules/metrics.py
+++ python-modules/metrics.py
@@ -3,6 +3,9 @@ import numpy as np
 
 # **********************❗❗❗❗❗❗**********************
 # Note: lower metric values are considered better
+
+# df1 is the original dataset
+# df2 is the transformed dataset
 # *********************************************************
 
 # qualitative without none
@@ -46,14 +49,34 @@ def qualitative(df1, df2):
     return np.average(ds,weights=ws)
 
 
+def non_none_percentage(df1, df2) -> float:
+    df1_non_none_cells = df1.count().sum()
+    df2_non_none_cells = df2.count().sum()
+
+    return df2_non_none_cells / df1_non_none_cells
+
+
 def quantitative(df1, df2):
     return 1 - non_none_percentage(df1, df2)
 
 
-def non_none_percentage(df1, df2) -> float:
-    # df1 is the original dataset
-    df1_non_none_cells = df1.count().sum()
-    # df2 is the filtered dataset
-    df2_non_none_cells = df2.count().sum()
+def _get_column_entropy(column) -> float:
+    # necessary to avoid 0-rounded elements
+    prob_offset = 1e-10
+    column_probability = column.value_counts(normalize=True, dropna=True)
+    return -sum(column_probability * np.log2(column_probability + prob_offset))
 
-    return df2_non_none_cells / df1_non_none_cells
+
+def _get_dataset_entropy(dataset) -> float:
+    entropies = [_get_column_entropy(dataset[column]) for column in dataset.columns ]
+    return np.mean(entropies)
+
+
+def entropy(df1, df2):
+    original_entropy = _get_dataset_entropy(df1)
+    new_entropy = _get_dataset_entropy(df2)
+
+    return abs(original_entropy - new_entropy)
+
+
+
